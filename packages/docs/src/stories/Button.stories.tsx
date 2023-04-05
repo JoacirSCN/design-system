cc845bd0fed72296bcccb38b49f5f517a12ca1a3
joacir <joacirprog@gmail.com>
2023-04-05 11:07:44 -0300

    problema resolvido

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+  /* Se eu passar o args aqui, o Primary e secondary herdam essa prop */
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+/* export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Enviar',
+  },
+}
+ */
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
