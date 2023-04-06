693069e3a25f14c9df80958b831fa27c7c07161c
joacir <joacirprog@gmail.com>
2023-04-06 11:28:54 -0300

    run all paste

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   /* Se eu passar o args aqui, o Primary e secondary herdam essa prop */
   args: {
