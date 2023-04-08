716f193895b8fae86ac42b7c5e8f5fa18884d002
joacir <joacirprog@gmail.com>
2023-04-08 15:45:52 -0300

    finished components

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,27 +1,50 @@
 import type { Meta, StoryObj } from '@storybook/react'
-
+import { ArrowRight } from 'phosphor-react'
 import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
   title: 'Form/Button',
   component: Button,
-  /* Se eu passar o args aqui, o Primary e secondary herdam essa prop */
   args: {
-    children: 'Enviar',
+    children: 'Send',
   },
 } as Meta<ButtonProps>
 
-/* export const Primary: StoryObj<ButtonProps> = {
-  args: {
-    children: 'Enviar',
-  },
-}
- */
-
 export const Primary: StoryObj<ButtonProps> = {}
 
-export const Big: StoryObj<ButtonProps> = {
+export const Secondary: StoryObj<ButtonProps> = {
   args: {
-    size: 'big',
+    variant: 'secondary',
+    children: 'Create New',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
   },
 }
