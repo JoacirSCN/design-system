a7fea28fa978896aadd490b12ca96bd94457dfbf
joacir <joacirprog@gmail.com>
2023-04-11 10:56:16 -0300

    error

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,5 +1,7 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
+/* * @type { import('@storybook/react-vite').StorybookConfig } */
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig  = {
   stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
@@ -7,9 +9,12 @@ const config = {
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite"
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
   },
   async viteFinal(config, { configType }) {
     if (configType === 'PRODUCTION') {
